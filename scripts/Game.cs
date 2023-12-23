f025d539cb250aacd9db441181efd203ca99d90b
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-12-22 18:31:11 -0600

    multiplayer movement and player spawnin

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,15 +1,17 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Game : Node2D
 {
+	[Export] public PackedScene PlayerScene;
+
 	private ENetMultiplayerPeer _peer;
+	private bool _host;
 
 	public override void _Ready()
 	{
-		Multiplayer.PeerConnected += (id) => GD.Print("Network peer connected " + id);
-		Multiplayer.ConnectedToServer += () => GD.Print("Connected to server");
-		Multiplayer.ConnectionFailed += () => GD.Print("Connection failed");
+		Multiplayer.PeerConnected += (id) => OnPeerConnected(id);
 
 		if (!Host()) Join("127.0.0.1");
 	}
@@ -27,6 +29,8 @@ public partial class Game : Node2D
 		_peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 
 		Multiplayer.MultiplayerPeer = _peer;
+
+		_host = true;
 
 		return true;
 	}
@@ -49,5 +53,27 @@ public partial class Game : Node2D
 	private void OnPeerConnected(long id)
 	{
 		GD.Print("Peer connected " + id);
+
+		if (!_host) return;
+
+		List<int> peers = new List<int>(Multiplayer.GetPeers())
+		{
+			1
+		};
+
+		Rpc(nameof(StartRpc), peers.ToArray());
+	}
+
+	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = true)]
+	private void StartRpc(int[] peers)
+	{
+		foreach (int peerId in peers)
+		{
+			Node2D player = PlayerScene.Instantiate<Node2D>();
+
+			AddChild(player);
+
+			player.GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer").SetMultiplayerAuthority(peerId);
+		}
 	}
 }
---
+++ scripts/Player.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public override void _PhysicsProcess(double delta)
+	{
+		if (GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer").GetMultiplayerAuthority() != Multiplayer.GetUniqueId()) return;
+
+		Vector2 movement = Vector2.Right * Input.GetAxis("move_left", "move_right") + Vector2.Up * Input.GetAxis("move_down", "move_up");
+
+		// GD.Print(movement);
+
+		Velocity = movement * 100f;
+
+		MoveAndSlide();
+	}
+}
