3d9f34b37dfa215ec8744eee85f3a3db585668f3
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-12-31 19:27:05 -0600

    Improve Menu

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -3,17 +3,34 @@ using Networking;
 
 public partial class MainMenu : Control
 {
+	[Export] public TextEdit ipAdressInput;
+	[Export] public Button hostButton;
+	[Export] public Button joinButton;
+	[Export] public Button startButton;
+
 	public void Host()
 	{
 		NetworkManager.Host();
 
-		QueueFree();
+		hostButton.QueueFree();
+		joinButton.QueueFree();
 	}
 
 	public void Join()
 	{
 		// NetworkManager.Join("127.0.0.1");
-		NetworkManager.Join("104.33.194.150");
+		// NetworkManager.Join("104.33.194.150");
+
+		NetworkManager.Join(ipAdressInput.Text);
+
+		QueueFree();
+	}
+
+	public void Start()
+	{
+		if (!NetworkManager.IsHost) return;
+
+		Game.Start();
 
 		QueueFree();
 	}
