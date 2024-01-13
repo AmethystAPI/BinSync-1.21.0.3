c39908a46aec960028cd5a0975a982525790ef67
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-01-13 03:19:28 -0600

    Fix Crow Shadow

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -1,11 +1,11 @@
 using Godot;
 
-public partial class Camera : Camera2D
-{
-	public override void _PhysicsProcess(double delta)
-	{
+public partial class Camera : Camera2D {
+	public override void _PhysicsProcess(double delta) {
 		if (Player.LocalPlayer == null) return;
 
-		GlobalPosition = Player.LocalPlayer.GlobalPosition;
+		Vector2 mouseOffset = GetGlobalMousePosition() - Player.LocalPlayer.GlobalPosition;
+
+		GlobalPosition = Player.LocalPlayer.GlobalPosition + mouseOffset / 6f;
 	}
 }
