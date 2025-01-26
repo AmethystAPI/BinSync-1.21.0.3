9f90be6f9b546dc503e8c4b2750e5495981a2915
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-01-26 14:06:56 -0600

    Move a lot of scripts around

--- scripts/rooms/DecorationPool.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-[Tool]
-public partial class DecorationPool : Resource {
-    [Export] public PackedScene[] Scenes;
-    [Export] public float Spacing = 100;
-    [Export] public float MinimumSpacing = 0;
-    [Export] public bool Rotate = false;
-}
