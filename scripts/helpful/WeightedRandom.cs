d596528870e6f56fd0bedab742cf534b707326e2
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-01-26 22:50:48 -0600

    Add weighted random

---
+++ scripts/helpful/WeightedRandom.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WeightedRandom<ValueType> {
+    private ValueType[] _values;
+    private float[] _weights;
+    private Func<float> _random;
+
+    public WeightedRandom(ValueType[] values, float[] weights, Func<float> random) {
+        _values = values;
+        _weights = weights;
+        _random = random;
+    }
+
+    public ValueType Get() {
+        float sum = _weights.Sum();
+
+        float target = _random() * sum;
+
+        float indexSum = 0;
+
+        for (int index = 0; index < _values.Length; index++) {
+            indexSum += _weights[index];
+
+            if (target <= indexSum) return _values[index];
+        }
+
+        return _values[_values.Length - 1];
+    }
+
+    public static ValueType Get(ValueType[] values, float[] weights, Func<float> random) {
+        WeightedRandom<ValueType> weightedRandom = new WeightedRandom<ValueType>(values, weights, random);
+
+        return weightedRandom.Get();
+    }
+}
